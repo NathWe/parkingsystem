35a327ab48e04c974177ebda92d1931e3b06d187
natha <natha@DESKTOP-NQ1L2U2.lan>
2022-12-29 13:05:45 +0100

    Modification tests

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -72,7 +72,7 @@ public class ParkingDataBaseIT {
 		final ParkingSpot parkingSpot = ticket.getParkingSpot();
 		assertFalse(parkingSpot.isAvailable());
 
-		// TODO: check that a ticket is actually saved in DB and Parking table is
+		// check that a ticket is actually saved in DB and Parking table is
 		// updated with availability
 	}
 
@@ -86,65 +86,8 @@ public class ParkingDataBaseIT {
 		assertNotNull(ticket.getOutTime());
 		assertNotNull(ticket.getPrice());
 
-		// TODO: check that the fare generated and out time are populated correctly in
+		// check that the fare generated and out time are populated correctly in
 		// the database
 	}
-
-
-    @Test
-    public void testParkingACar2() throws Exception{
-        final ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
-        
-        con = dataBaseTestConfig.getConnection();
-
-		String sql = "SELECT VEHICLE_REG_NUMBER FROM ticket WHERE ID=?";
-		PreparedStatement statement = con.prepareStatement(sql);
-		statement.setInt(1, 1);
-		ResultSet rs = statement.executeQuery();
-		rs.next();
-
-		String sql1 = "SELECT AVAILABLE FROM parking WHERE PARKING_NUMBER=?";
-		PreparedStatement statement1 = con.prepareStatement(sql1);
-		statement1.setInt(1, 1);
-		ResultSet rs1 = statement1.executeQuery();
-		rs1.next();
-
-		
-		// assertThat(rs.getString(1)).isEqualTo("ABCDEF");
-
-		// assertThat(rs1.getInt(1)).isEqualTo(0);
+	
     }
-
-    @Test
-    public void testParkingLotExit2() throws Exception{
-        testParkingACar1();
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
-        
-        TimeUnit.MILLISECONDS.sleep(500);
-		parkingService.processExitingVehicle();
-		
-		con = dataBaseTestConfig.getConnection();
-
-		String sql2 = "SELECT PRICE FROM ticket WHERE ID=?";
-		PreparedStatement statement2 = con.prepareStatement(sql2);
-		statement2.setInt(1, 1);
-		ResultSet rs2 = statement2.executeQuery();
-		rs2.next();
-
-		String sql3 = "SELECT OUT_TIME FROM ticket WHERE ID=?";
-		PreparedStatement statement3 = con.prepareStatement(sql3);
-		statement3.setInt(1, 1);
-		ResultSet rs3 = statement3.executeQuery();
-		rs3.next();
-
-		
-		// assertThat(rs2.getDouble(1)).isEqualTo(ticketDAO.getTicket("ABCDEF").getPrice());
-
-		// assertThat(rs3.getTimestamp(1)).isEqualTo(ticketDAO.getTicket("ABCDEF").getOutTime());
-		
-		
-    }}
